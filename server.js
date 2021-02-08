e9c7c095375bc0cecefabf78fb86de761b85f56a
melissa-stott <1princessmelissa@gmail.com>
2021-02-07 17:14:53 -0800

    added code to connect server.js to mySQL workbench

--- server.js
+++ server.js
@@ -2,3 +2,18 @@ const express = require('express');
 const mysql = require('mysql');
 const inquirer = require('inquirer')
 const PORT = process.env.PORT || 8080;
+const app = express();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '5r00+sql',
+    database: 'workforce_DB',
+  });
+  
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+  });
+  app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
