f0426306c5ccad2d2c449dffd030b0986d05bd5c
melissa-stott <1princessmelissa@gmail.com>
2021-02-07 16:43:37 -0800

    fixed errors in schema syntax

--- TeamSchema.sql
+++ TeamSchema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS workforce_DB;
 
 CREATE DATABASE workforce_DB;
 
-USE DATABASE workforce_DB;
+USE workforce_DB;
 
 CREATE TABLE department (
     id INTEGER NOT NULL AUTO_INCREMENT,
@@ -15,15 +15,14 @@ CREATE TABLE roles (
     title VARCHAR (30) NOT NULL,
     salary DECIMAL (10,2) NOT NULL,
     department_id INTEGER NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
     id INTEGER NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL
+    first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
-    role_id INTEGER NOT NULL;
+    role_id INTEGER NOT NULL,
     manager_id INTEGER,
     PRIMARY KEY (id)
 );
