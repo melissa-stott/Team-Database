18dfef908a1124cb8c435ed1337c523a06ab1d43
melissa-stott <1princessmelissa@gmail.com>
2021-02-08 20:49:18 -0800

    created opening query, and set up switch statement

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const { response } = require('express');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
@@ -17,3 +18,31 @@ const connection = mysql.createConnection({
     console.log(`connected as id ${connection.threadId}`);
   });
   app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+  const firstQuery = () => {
+      inquirer.prompt(
+          {
+              type: 'list',
+              name: 'startPoint',
+              message: 'What would you like to do?',
+              choices: ['View data', 'Update data', 'Add data', 'Exit'],
+          }
+      ).then((answer) => {
+          switch (answer.startPoint) {
+              case 'View data':
+                  viewInfo();
+                  break;
+              case 'Update data':
+                  updateInfo();
+                  break;
+              case 'Add data':
+                  addInfo();
+                  break;
+              case 'Exit':
+                  connection.end();
+                  break;          
+              }
+      })
+  }
+
+  
